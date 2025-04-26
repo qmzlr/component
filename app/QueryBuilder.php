02c3551baabf88f42333f92c807e9dc34526d1c5
qmzlr <drozdovvalerij61@gmail.com>
2025-04-26 12:44:52 +0400

    QueryBuilder, route and php di

---
+++ app/QueryBuilder.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App;
+
+use Aura\SqlQuery\QueryFactory;
+use PDO;
+
+class QueryBuilder
+{
+    private PDO $pdo;
+    private QueryFactory $queryFactory;
+
+    public function __construct(PDO $pdo, QueryFactory $queryFactory)
+    {
+        $this->pdo = $pdo;
+        $this->queryFactory = $queryFactory;
+    }
+
+    public function getAll($table): array | false
+    {
+        $select = $this->queryFactory->newSelect();
+        $select->cols(['*'])
+            ->from($table);
+        $sth = $this->pdo->prepare($select->getStatement());
+        $sth->execute($select->getBindValues());
+        return $sth->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    public function insert($table, $data): void
+    {
+        $insert = $this->queryFactory->newInsert()
+            ->into($table)
+            ->cols($data);
+        $sth = $this->pdo->prepare($insert->getStatement());
+        $sth->execute($insert->getBindValues());
+    }
+
+    public function update($table, $data, $where): void
+    {
+        $update = $this->queryFactory->newUpdate()
+            ->table($table)
+            ->cols($data)
+            ->where('id = :id', ['id' => $where]);
+        $sth = $this->pdo->prepare($update->getStatement());
+        $sth->execute($update->getBindValues());
+    }
+
+    public function delete($table, $where): void
+    {
+        $delete = $this->queryFactory->newDelete()
+            ->from($table)
+            ->where('id = :id', ['id' => $where]);
+        $sth = $this->pdo->prepare($delete->getStatement());
+        $sth->execute($delete->getBindValues());
+    }
+}
--- public/index.php
+++ public/index.php
@@ -1,7 +1,63 @@
 <?php
+
+use Aura\SqlQuery\QueryFactory;
+use DI\ContainerBuilder;
+
 if (!session_id()) {
     session_start();
 }
 
 require '../vendor/autoload.php';
 
+
+$builder = new ContainerBuilder();
+$builder->addDefinitions([
+
+    PDO::class => function () {
+        $driver = 'mysql';
+        $host = 'localhost';
+        $database = 'vmind';
+        $username = 'root';
+        $password = '';
+        return new PDO("$driver:host=$host;dbname=$database", $username, $password);
+    },
+    QueryFactory::class => function () {
+        return new QueryFactory('mysql');
+    }
+
+]);
+
+
+try {
+    $container = $builder->build();
+} catch (Exception $e) {
+    echo $e->getMessage();
+    exit;
+}
+
+$dispatcher = FastRoute\simpleDispatcher(function (FastRoute\RouteCollector $r) {
+
+});
+
+$httpMethod = $_SERVER['REQUEST_METHOD'];
+$uri = $_SERVER['REQUEST_URI'];
+
+if (false !== $pos = strpos($uri, '?')) {
+    $uri = substr($uri, 0, $pos);
+}
+$uri = rawurldecode($uri);
+
+
+$routeInfo = $dispatcher->dispatch($httpMethod, $uri);
+switch ($routeInfo[0]) {
+    case FastRoute\Dispatcher::NOT_FOUND:
+        echo '404 Not Found';
+        break;
+    case FastRoute\Dispatcher::METHOD_NOT_ALLOWED:
+        $allowedMethods = $routeInfo[1];
+        echo '405 Method Not Allowed';
+        break;
+    case FastRoute\Dispatcher::FOUND:
+        $controller = $container->call($routeInfo[1], [$routeInfo[2] + $_GET + $_POST]);
+        break;
+}
